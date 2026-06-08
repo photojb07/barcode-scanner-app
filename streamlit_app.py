59c1d0f23dac7ba7eed3e9830a6ced30d45935ea
photojb07 <photo.jb07@gmail.com>
2026-06-08 19:04:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ def upload_to_stage(conn, file_bytes, filename):
         f.write(file_bytes)
     try:
         cursor = conn.cursor()
-        sql = "PUT file://" + tmp_path + " @BARCODE_UPLOADS.PUBLIC.IMAGE_STAGE AUTO_COMPRESS=FALSE SOURCE_COMPRESSION=NONE OVERWRITE=FALSE"
+        sql = "PUT file://" + tmp_path + " @BARCODE_UPLOADS.PUBLIC.IMAGE_STAGE AUTO_COMPRESS=FALSE SOURCE_COMPRESSION=NONE OVERWRITE=FALSE PARALLEL=1"
         cursor.execute(sql)
         cursor.close()
     finally:
